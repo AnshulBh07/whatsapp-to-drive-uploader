df712da25a496b8df7865870dc82524c938824dc
AnshulBh07 <anshulbh009@gmail.com>
2026-06-30 16:49:50 +0530

    [ADD] : drive upload debug consoles

--- src/whatsapp/events.ts
+++ src/whatsapp/events.ts
@@ -36,48 +36,52 @@ export const registerEvents = (socket: WASocket) => {
 		if (type !== "notify") return;
 
 		for (const msg of messages) {
-			if (msg.key.remoteJid !== process.env.PRESCRIPTION_GROUP_ID) continue;
+			try {
+				if (msg.key.remoteJid !== process.env.PRESCRIPTION_GROUP_ID) continue;
 
-			if (!msg.message?.imageMessage) continue;
+				if (!msg.message?.imageMessage) continue;
 
-			// check if it is a duplicate with mongodb check
-			const pres = await UploadedPrescription.findOne({
-				messageId: msg.key.id,
-			});
+				// check if it is a duplicate with mongodb check
+				const pres = await UploadedPrescription.findOne({
+					messageId: msg.key.id,
+				});
 
-			if (pres) continue;
+				if (pres) continue;
 
-			// create image buffer
-			const buffer = await downloadMedia(socket, msg);
+				// create image buffer
+				const buffer = await downloadMedia(socket, msg);
 
-			const timestamp = msg.messageTimestamp
-				? Number(msg.messageTimestamp) * 1000
-				: Date.now();
+				const timestamp = msg.messageTimestamp
+					? Number(msg.messageTimestamp) * 1000
+					: Date.now();
 
-			const fileName = `prescription_${timestamp}_${msg.key.id}.jpg`;
+				const fileName = `prescription_${timestamp}_${msg.key.id}.jpg`;
 
-			// upload it to drive
-			const uploaded = await uploadToDrive({
-				parentFolderId: process.env.GOOGLE_DRIVE_ROOT_FOLDER_ID!,
-				filename: fileName,
-				mimeType: "image/jpeg",
-				buffer,
-			});
+				// upload it to drive
+				const uploaded = await uploadToDrive({
+					parentFolderId: process.env.GOOGLE_DRIVE_ROOT_FOLDER_ID!,
+					filename: fileName,
+					mimeType: "image/jpeg",
+					buffer,
+				});
 
-			await UploadedPrescription.create({
-				messageId: msg.key.id!,
-				driveFileId: uploaded.id!,
-				filename: fileName,
-				sender: msg.key.participant,
-			});
+				await UploadedPrescription.create({
+					messageId: msg.key.id!,
+					driveFileId: uploaded.id!,
+					filename: fileName,
+					sender: msg.key.participant,
+				});
 
-			// await saveImage(buffer, fileName);
-			// console.log({
-			// 	chat: msg.key.remoteJid,
-			// 	fromMe: msg.key.fromMe,
-			// 	participant: msg.key.participant,
-			// 	message: msg.message,
-			// });
+				// await saveImage(buffer, fileName);
+				// console.log({
+				// 	chat: msg.key.remoteJid,
+				// 	fromMe: msg.key.fromMe,
+				// 	participant: msg.key.participant,
+				// 	message: msg.message,
+				// });
+			} catch (err) {
+				console.error("❌ Failed processing message : ", msg.key.id, err);
+			}
 		}
 	});
 };
